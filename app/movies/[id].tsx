b7452c86a687c84c99b244861609951f55cf02c6
Onukwu Ifeanyichukwu Boluwatife <bolu.onukwu@gmail.com>
2026-06-22 12:59:39 +0100

    New Updates On MovieFlix

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const MovieDetails = () => {
-  return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
-  )
-}
+  const { id } = useLocalSearchParams();
 
-export default MovieDetails
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string),
+  );
+
+  return (
+    <View className="bg-primary flex-1">
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 80,
+        }}
+      >
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+          />
+          <Text className="text-white text-2xl font-bold">{movie?.title}</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MovieDetails;
